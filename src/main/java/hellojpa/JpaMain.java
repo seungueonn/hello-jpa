0c45e0bd1fb2e41929f8d67638e3acc0a7bf2947
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-07 03:44:26 +0900

    ☑️ 순수 JPA 로 데이터 저장 성공

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,13 +2,27 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.close();
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("helloA");
+
+        em.persist(member);
+
+        tx.commit();
+
+        em.close();
+
         emf.close();
 
     }
