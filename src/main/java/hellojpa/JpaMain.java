a991a638f107aa50f56f340524d24a56fe34edcc
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-07 23:08:36 +0900

    🔄 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,14 +16,19 @@ public class JpaMain {
 
         try {
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class) //jpql
-                    .setFirstResult(1)
-                    .setMaxResults(10) //pagination
-                    .getResultList();
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("helloJPA");
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            System.out.println("======BEFORE======");
+            em.persist(member);
+            System.out.println("======BEFORE======");
+
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember = " + findMember.getId());
+            System.out.println("findMember = " + findMember.getName());
+            // 조회 성공. em.persist() 이후 영속 컨텍스트로 이동, 1차 캐시에 저장, 1차캐시에서 불러옴
+
 
             // TODO : create
 //            Member member = new Member();
