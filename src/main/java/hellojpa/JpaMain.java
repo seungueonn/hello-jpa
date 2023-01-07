7565276554dfd110580abb0956d794bf856ee617
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-08 00:52:58 +0900

    ☑️ 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,38 +16,6 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setId(102L);
-            member.setName("helloJPA");
-
-            System.out.println("======BEFORE======");
-            em.persist(member);
-            System.out.println("======AFTER======");
-
-            Member findMember = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에 있음, 조회 sql 안뜸
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
-            // 조회 성공. em.persist() 이후 영속 컨텍스트로 이동, 1차 캐시에 저장, 1차캐시에서 불러옴
-
-
-            // TODO : create
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("helloB");
-//
-//            em.persist(member);
-
-            // TODO : read
-//            Member findMember = em.find(Member.class, 1L);
-
-            // TODO : update
-
-//            findMember.setName("helloJPA");
-
-            // TODO : delete
-//            em.remove(findMember);
-
             tx.commit(); //쓰기 지연
 
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 //@Table(name="USER")
@@ -11,31 +9,28 @@ public class Member {
 
     @Id
     private Long id;
-    @Column(unique = true,length = 10)
-    private String name;
-    private int age;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Member() {
-        //jpa 객체는 기본 생성자 필요
-    }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
