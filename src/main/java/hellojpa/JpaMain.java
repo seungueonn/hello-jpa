239c322197105ef648bd9dae5ef1fe70817a344f
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-14 18:05:31 +0900

    ☑️ em.getReference 프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import hellojpa.Member;
 import hellojpa.Team;
+import org.hibernate.Hibernate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,35 +16,43 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
 
-            // TODO : mappedSuperClass
             Member member = new Member();
-            member.setName("user");
-            member.setCreatedBy("Jeong");
-            member.setCreatedDate(LocalDateTime.now());
+            member.setName("hello");
 
             em.persist(member);
-
-            // TODO : 상속 관계 매핑
-            Movie movie = new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbb");
-            movie.setName("Avatar");
-            movie.setPrice(10000);
-            em.persist(movie);
 
             em.flush();
             em.clear();
 
-            Item findMovie = em.find(Item.class, movie.getId());
-            System.out.println("findMovie = " + findMovie.getName());
+            //application clear 와 같은 의미
+//          Member findMember = em.find(Member.class, member.getId());
+
+            Member findMember = em.getReference(Member.class, member.getId()); // 가짜(프록시) 엔티티 객체 조회
+            System.out.println("findMember = " + findMember.getId());
+            System.out.println("findMember = " + findMember.getName()); // 레퍼런스에 없네? 하고 select 쿼리 날림
+            // findMember는 계속 프록시 객체 프록시 유지.
+            //
+            em.flush();
+            em.clear();
+
+            System.out.println("findMember.getClass() = " + findMember.getClass());
+            System.out.println("isLoaded=" + emf.getPersistenceUnitUtil().isLoaded(findMember));
+
+            findMember.getName(); // 강제 초기화
+            System.out.println("isLoaded=" + emf.getPersistenceUnitUtil().isLoaded(findMember));
+
+            Hibernate.initialize(findMember); // 강제 초기화
 
             tx.commit();
+
+
 
         } catch (Exception e) {
             tx.rollback();
@@ -54,6 +63,17 @@ public class JpaMain {
 
     }
 
+    // Member를 조회할 때 Team도 함께 조회해야 할까?
+    private static void printMember(Member member) {
+        System.out.println("member.getName() = " + member.getName());
+    }
+
+    private static void printMemberAndTeam(Member member) {
+        String userName = member.getName();
+        System.out.println("userName = " + userName);
+        Team team = member.getTeam();
+        System.out.println("team = " + team.getName());
+    }
 
 
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,15 +15,39 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String name;
 
-//    @ManyToOne
-//    @JoinColumn(name = "TEAM_ID",insertable = false, updatable = false) // 야매
-//    private Team team;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID",insertable = false, updatable = false) // 야매
+    private Team team;
 
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
 
-//    @ManyToMany
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Locker getLocker() {
+        return locker;
+    }
+
+    public void setLocker(Locker locker) {
+        this.locker = locker;
+    }
+
+    public List<MemberProduct> getMemberProducts() {
+        return memberProducts;
+    }
+
+    public void setMemberProducts(List<MemberProduct> memberProducts) {
+        this.memberProducts = memberProducts;
+    }
+
+    //    @ManyToMany
 //    @JoinTable(name = "MEMBER_PRODUCT") // 다대다
     @OneToMany(mappedBy = "member")
     private List<MemberProduct> memberProducts = new ArrayList<>();
