1c0953f921073dbbad7ca7d78af37a5d009acd04
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-17 22:24:01 +0900

    ☑️ CASCADE : 영속성 전이

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,34 +23,18 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.flush();
-            em.clear();
+            em.persist(parent);
+//            em.persist(child1);
+//            em.persist(child2); // -> parent가 child를 관리, parent 할 때 child도 Persist 할 수 있도록 -> CASCADE
 
-            Member m = em.find(Member.class, member1.getId());
-
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
-
-            // Member 쿼리 + EAGER 즉시 가져오려고 쿼리 더
-            // SQL : select * from Member
-            // SQL : select * from team where TEAM_ID = xxx
-
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-
-            System.out.println("=======query========");
-            m.getTeam().getName();
-            System.out.println("=======query========");
             tx.commit();
-
 
 
         } catch (Exception e) {
