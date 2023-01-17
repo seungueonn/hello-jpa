916239b55dce166a0cf6d1a042f1816e5db5a0c5
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-18 02:36:07 +0900

    ☑️ 값 타입은 동등성equals overrride 비교

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -2,11 +2,9 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
-        Integer a = new Integer(10);
-        Integer b = a;
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
 
-        a = 20;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("address1==address2:"+ address1.equals(address2));
     }
 }
